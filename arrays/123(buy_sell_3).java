522cb6ccc19b880e54a1e372cd9abf5a9b9c6ae6
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-05-13 23:10:18 +0530

    13th May

--- arrays/123(buy_sell_3).java
+++ arrays/123(buy_sell_3).java
@@ -24,6 +24,24 @@ class arrays {
         return f(0, 1, 2, prices, dp);
     }
 
+    public int maxProfitTabulation(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n + 1][2][3];
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy == 1) {
+                        dp[ind][buy][cap] = Math.max((-prices[ind] + dp[ind + 1][0][cap]), (0 + dp[ind + 1][1][cap]));
+                    } else {
+                        dp[ind][buy][cap] = Math.max((prices[ind] + dp[ind + 1][1][cap - 1]), (0 + dp[ind + 1][0][cap]));
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+
     public static void main(String[] args) {
 
     }
