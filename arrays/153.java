e649139ce34a339b317fdad3dace51401edb3dba
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-05-20 12:02:04 +0530

    20th may

---
+++ arrays/153.java
@@ -0,0 +1,26 @@
+class arrays{
+    public int findMin(int[] nums){
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int ans = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] < ans){
+                ans = Math.min(ans, nums[mid]);
+            }
+            if(nums[low] < nums[mid]){
+                ans = Math.min(nums[low], ans);
+                low = mid + 1;
+            }else{
+                ans = Math.min(nums[mid], ans);
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
