e18ffd0a5ddc7c74fc5570a61caaaa4cd4b3b93a
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-06-14 02:48:17 +0530

    14th june

--- arrays/42(trapping_rainwater).java
+++ arrays/42(trapping_rainwater).java
@@ -22,6 +22,31 @@ class arrays {
         }
         return total;
     }
+    // asked in goldman sachs interview (trapping most snow in between buildings)
+    public int trapThing(int[] height){
+        int n = height.length;
+        int lmax = 0;
+        int rmax = 0;
+        int l = 0;
+        int r = n-1;
+        int total = 0;
+        while(l <= r){
+            if(height[l] < height[r]){
+                if(lmax < height[l]){
+                    lmax = Math.max(lmax, height[l]);
+                }
+                total += lmax - height[l];
+                l++;
+            }else{
+                if(rmax < height[r]){
+                    rmax = Math.max(rmax, height[r]);
+                }
+                total += rmax - height[r];
+                r--;
+            }
+        }
+        return total;
+    }
 
     public static void main(String[] args) {
 
