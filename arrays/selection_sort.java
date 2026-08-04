b77074dd7e77317a8102d7144ce6f8f96d881488
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-08-04 16:49:38 +0530

    selection sort

---
+++ arrays/selection_sort.java
@@ -0,0 +1,26 @@
+
+class Selection {
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
